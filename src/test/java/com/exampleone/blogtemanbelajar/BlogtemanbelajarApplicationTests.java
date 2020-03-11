e3d3476ee5daca51caa0c137db594dd51d87878c
leni_05 <leni@indosystem.com>
2020-03-11 17:04:30 +0700

    first commit

---
+++ src/test/java/com/exampleone/blogtemanbelajar/BlogtemanbelajarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleone.blogtemanbelajar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogtemanbelajarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
